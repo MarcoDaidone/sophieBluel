613b11f5dbd52476576585e68c62fa3903219e8e
Marcod <daidone.marco38280@gmail.com>
2023-11-03 14:45:19 +0100

    buton dynamics

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,7 +1,9 @@
 let allData = []; // Stocker les données ici
 
 function projetData(dataToRender) {
-     if (!dataToRender) {
+     if (dataToRender) {
+          renderData(dataToRender);
+     } else {
           fetch('http://localhost:5678/api/works')
                .then((response) => {
                     return response.json();
@@ -9,12 +11,11 @@ function projetData(dataToRender) {
                .then((data) => {
                     allData = data; // Stockez les données pour une utilisation ultérieure
                     renderData(data);
+                    createFilterButtons(allData);
                })
                .catch((error) => {
                     console.error('Error:', error);
                });
-     } else {
-          renderData(dataToRender);
      }
 }
 
@@ -31,42 +32,62 @@ function renderData(data) {
           let galleries = document.querySelectorAll('.gallery');
           galleries.forEach((gallery) => {
                let figureElement = document.createElement('figure');
-               figureElement.appendChild(image.cloneNode(true));
-               figureElement.appendChild(figCaption.cloneNode(true));
+               figureElement.appendChild(image);
+               figureElement.appendChild(figCaption);
                gallery.appendChild(figureElement);
           });
      });
+
+     let categories = data.map((item) => item.category.name);
+     let buttonsContainer = document.querySelector('.filter');
+     projetData.innerHTML = '';
+}
+
+function createFilterButtons(data) {
+     let categories = data.map((item) => item.category.name); // Utiliser Set pour obtenir des catégories uniques
+     let uniqueCat = new Set(categories);
+     let resetButon = document.createElement('a');
+     resetButon.textContent = 'Tous';
+     resetButon.classList.add('bouton');
+     const buttonsContainer = document.querySelector('.filter');
+     buttonsContainer.appendChild(resetButon);
+
+     // Définir la fonction qui gère la classe 'active'
+     function setActiveButton(activeButton) {
+          document.querySelectorAll('.filter .bouton').forEach((btn) => {
+               btn.classList.remove('active');
+          });
+          // Ajouter la classe 'active' au bouton cliqué
+          if (activeButton) {
+               activeButton.classList.add('active');
+          }
+     }
+
+     resetButon.addEventListener('click', () => {
+          resetFilter();
+          setActiveButton(resetButon);
+     });
+     // Créer un bouton pour chaque catégorie unique
+     uniqueCat.forEach((category) => {
+          let button = document.createElement('a');
+          button.textContent = category;
+          button.classList.add('bouton');
+          button.addEventListener('click', () => {
+               filterByCategory(category);
+               setActiveButton(button);
+          });
+          buttonsContainer.appendChild(button);
+     });
+
+     function filterByCategory(categoryName, clickedButton) {
+          const filteredData = allData.filter(
+               (object) => object.category.name === categoryName
+          );
+          projetData(filteredData);
+     }
+     function resetFilter(categoryName) {
+          renderData(data);
+     }
 }
 
 projetData();
-
-// Filter button logic
-const boutonElement = document.querySelector('.bouton_filtre');
-const boutonAll = document.querySelector('.bouton_All');
-const boutonAppartements = document.querySelector('.bouton_Appartements');
-const boutonHotel = document.querySelector('.bouton_Hotel');
-
-boutonElement.addEventListener('click', function (event) {
-     const filteredData = allData.filter(
-          (object) => object.category.name === 'Objets'
-     );
-     projetData(filteredData);
-});
-
-boutonAll.addEventListener('click', function (event) {
-     projetData();
-});
-
-boutonHotel.addEventListener('click', function (event) {
-     const filteredData = allData.filter(
-          (object) => object.category.name === 'Hotels & restaurants'
-     );
-     projetData(filteredData);
-});
-
-boutonAppartements.addEventListener('click', function (event) {
-     const filteredData = allData.filter(
-          (object) => object.category.name === 'Appartements'
-     );
-     projetData(filteredData);
-});
