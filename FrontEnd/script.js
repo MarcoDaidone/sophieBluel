570095202e15d3803ae96bec22c6c42910962a79
Marcod <daidone.marco38280@gmail.com>
2023-11-13 08:50:37 +0100

    intégration login page + logic connexion

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,25 +1,25 @@
 let allData = []; // Stocker les données ici
 
 function projetData(data) {
-     if (data) {
-          renderData(data);
-     } else {
-          fetch('http://localhost:5678/api/works')
-               .then((response) => {
-                    return response.json();
-               })
-               .then((data) => {
-                    allData = data; // Stockez les données pour une utilisation ultérieure
-                    renderData(data);
-                    createFilterButtons(data);
-               })
-               .catch((error) => {
-                    console.error('Error:', error);
-               });
+     if (window.location.pathname.includes('index')) {
+          if (data) {
+               renderData(data);
+          } else {
+               fetch('http://localhost:5678/api/works')
+                    .then((response) => {
+                         return response.json();
+                    })
+                    .then((data) => {
+                         allData = data; // Stockez les données pour une utilisation ultérieure
+                         renderData(data);
+                         createFilterButtons(data);
+                    })
+                    .catch((error) => {
+                         console.error('Error:', error);
+                    });
+          }
      }
 }
-
-//
 
 function renderData(data) {
      document.querySelector('.gallery').innerHTML = ''; // Vider la galerie avant de rendre
@@ -46,6 +46,27 @@ function renderData(data) {
 }
 // pas de fonction dans une fonction (mettre ça au propre)
 
+// Définir la fonction qui gère la classe 'active'
+function setActiveButton(activeButton) {
+     document.querySelectorAll('.filter .bouton').forEach((btn) => {
+          btn.classList.remove('active');
+     });
+     // Ajouter la classe 'active' au bouton cliqué
+     if (activeButton) {
+          activeButton.classList.add('active');
+     }
+}
+function filterByCategory(categoryName, clickedButton) {
+     const filteredData = allData.filter(
+          (object) => object.category.name === categoryName
+     );
+     projetData(filteredData);
+}
+function resetFilter(categoryName) {
+     const resetData = allData;
+     projetData(resetData);
+}
+
 function createFilterButtons(data) {
      let categories = data.map((item) => item.category.name); // Utiliser Set pour obtenir des catégories uniques
      let uniqueCat = new Set(categories);
@@ -54,17 +75,6 @@ function createFilterButtons(data) {
      resetButon.classList.add('bouton');
      const buttonsContainer = document.querySelector('.filter');
      buttonsContainer.appendChild(resetButon);
-
-     // Définir la fonction qui gère la classe 'active'
-     function setActiveButton(activeButton) {
-          document.querySelectorAll('.filter .bouton').forEach((btn) => {
-               btn.classList.remove('active');
-          });
-          // Ajouter la classe 'active' au bouton cliqué
-          if (activeButton) {
-               activeButton.classList.add('active');
-          }
-     }
 
      resetButon.addEventListener('click', () => {
           resetFilter();
@@ -81,16 +91,36 @@ function createFilterButtons(data) {
           });
           buttonsContainer.appendChild(button);
      });
-
-     function filterByCategory(categoryName, clickedButton) {
-          const filteredData = allData.filter(
-               (object) => object.category.name === categoryName
-          );
-          projetData(filteredData);
-     }
-     function resetFilter(categoryName) {
-          renderData(data);
-     }
 }
 
 projetData();
+
+// formulaire de connexion
+if (window.location.pathname.includes('login')) {
+     document.getElementById('login').addEventListener('submit', function (e) {
+          e.preventDefault();
+
+          const email = document.getElementById('email').value;
+          const password = document.getElementById('password').value;
+
+          console.log(email, password);
+
+          // Envoi de la requête POST
+          fetch('http://localhost:5678/api/users/login', {
+               method: 'POST',
+               body: JSON.stringify({
+                    email: email,
+                    password: password,
+               }),
+          })
+               .then((response) => response.json())
+               .then((data) => {
+                    console.log('Succès:', data);
+                    // Gérez ici la logique après une connexion réussie
+               })
+               .catch((error) => {
+                    console.error('Erreur:', error);
+                    // Gérez ici les erreurs
+               });
+     });
+}
